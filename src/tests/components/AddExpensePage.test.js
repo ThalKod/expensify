033a79453f827cd8667ea1de90f27a4745aecda6
ThalKod <tmarcelin@thal.tech>
2018-07-14 13:26:47 -0400

    add AddExpensePage component test suits

---
+++ src/tests/components/AddExpensePage.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { AddExpensePage } from "../../components/AddExpensePage";
+import expenses from "../fixtures/expenses";
+
+let onSubmit;
+let history;
+let wrapper;
+
+
+beforeEach(()=>{
+    onSubmit = jest.fn();
+    history = { push: jest.fn() };
+    wrapper  = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
+});
+
+test("should render AddExpensePAge correctly", ()=>{
+    expect(wrapper).toMatchSnapshot();
+});
+
+test("should handle onSubmit",  ()=>{
+    wrapper.find("ExpenseForm").prop("onSubmit")(expenses[0]);
+    expect(history.push).toHaveBeenLastCalledWith("/");
+    expect(onSubmit).toHaveBeenCalledWith(expenses[0]);
+});
