a73521e2f48d332f23494fe7e185ea433aeb5512
ThalKod <tmarcelin@thal.tech>
2018-08-04 14:07:47 -0400

    setup test database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from "firebase";
 
 const config = {
-    apiKey: process.env.FAPI_KEY,
-    authDomain: "expensify-b3bd9.firebaseapp.com",
-    databaseURL: "https://expensify-b3bd9.firebaseio.com",
-    projectId: "expensify-b3bd9",
-    storageBucket: "expensify-b3bd9.appspot.com",
-    messagingSenderId: "12038575609"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   };
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 const path = require("path");
+const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+process.env.NODE_ENV = process.env.NODE_ENV || "developement";
+
+if(process.env.NODE_ENV === "test"){
+    require("dotenv").config({ path: ".env.test" });
+}else if (process.env.NODE_ENV === "developement"){
+    require("dotenv").config({ path: ".env.developement" });
+}
 
 module.exports = (env)=>{
     const isProduction =  env === "production";
     const CSSExtract = new ExtractTextPlugin("style.css");
-
+ 
     return {
         entry: "./src/app.js",
         output: {
@@ -32,7 +41,16 @@ module.exports = (env)=>{
                 // both options are optional
                 filename:  "style.css", // devMode ? 'style.css' : 'style.[hash].css',
                 chunkFilename: "[id].css"  // devMode ? '[id].css' : '[id].[hash].css',
+              }),
+              new webpack.DefinePlugin({
+                  "process.env.FIREBASE_API_KEY": JSON.stringify(process.env.FIREBASE_API_KEY), 
+                  "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN), 
+                  "process.env.FIREBASE_DATABASE": JSON.stringify(process.env.FIREBASE_DATABASE), 
+                  "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET), 
+                  "process.env.FIREBASE_PROJECT_ID": JSON.stringify(process.env.FIREBASE_PROJECT_ID), 
+                  "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID) 
               })
+
         ],
         devtool: isProduction ? "source-map" : "inline-source-map",
         devServer: {
