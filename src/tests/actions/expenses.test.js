3045fefc678d92fa64dd0f65b3b3631302040d21
ThalKod <tmarcelin@thal.tech>
2018-08-13 13:07:33 -0400

    store users data in users area

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -4,6 +4,8 @@ import { addExpense, startAddExpense, editExpense, removeExpense, setExpenses, s
 import expenses from "../fixtures/expenses";
 import database from "../../firebase/firebase";
 
+const uid = "testuid";
+const defaulAuthState = {auth: { uid }};
 const createMockStore = configureMockStore([thunk]);
 
 beforeEach((done)=>{
@@ -11,7 +13,7 @@ beforeEach((done)=>{
     expenses.forEach(({ id, description, note, amount, createAt })=>{
         expensesData[id] = { description, note, amount, createAt };
     });
-    database.ref("expenses").set(expensesData).then(() => done());
+    database.ref(`users/${uid}/expenses`).set(expensesData).then(() => done());
 })
 
 
@@ -47,7 +49,7 @@ test("Should setup addExpense action object", ()=>{
 
 
 test("should add expense to database and store", (done)=>{
-    const store = createMockStore({});
+    const store = createMockStore(defaulAuthState);
 
     const expenseData = {
         description: "Mousse",
@@ -66,7 +68,7 @@ test("should add expense to database and store", (done)=>{
             }
         });
 
-        return database.ref(`expenses/${actions[0].expense.id}`).once("value");
+        return database.ref(`users/${uid}/expenses/${actions[0].expense.id}`).once("value");
         
     }).then((snapshot)=>{
         expect(snapshot.val()).toEqual(expenseData);
@@ -75,7 +77,7 @@ test("should add expense to database and store", (done)=>{
 });
 
 test("should add expense with defaults to database and store", (done)=>{
-    const store = createMockStore({});
+    const store = createMockStore(defaulAuthState);
 
     const dataDefault = {
         description : "", 
@@ -94,7 +96,7 @@ test("should add expense with defaults to database and store", (done)=>{
             }
         });
 
-        return database.ref(`expenses/${actions[0].expense.id}`).once("value");
+        return database.ref(`users/${uid}/expenses/${actions[0].expense.id}`).once("value");
         
     }).then((snapshot)=>{
         expect(snapshot.val()).toEqual(dataDefault);
@@ -111,7 +113,7 @@ test("should setup set expense action object with data", ()=>{
 });
 
 test("should fetch the expenses from firebase", (done)=>{
-    const store = createMockStore({});
+    const store = createMockStore(defaulAuthState);
     store.dispatch(startSetExpenses()).then(()=>{
         const action = store.getActions();
 
@@ -125,7 +127,7 @@ test("should fetch the expenses from firebase", (done)=>{
 });
 
 test("should remove expense from database", (done)=>{
-    const store = createMockStore({});
+    const store = createMockStore(defaulAuthState);
     store.dispatch(startRemoveExpense({ id: expenses[0].id })).then(()=>{
         const action = store.getActions();
 
@@ -134,7 +136,7 @@ test("should remove expense from database", (done)=>{
             id: expenses[0].id
         });
 
-        return database.ref(`expenses/${expenses[0].id}`).once("value").then((snapshot)=>{
+        return database.ref(`users/${uid}/expenses/${expenses[0].id}`).once("value").then((snapshot)=>{
             expect(snapshot.val()).toBeFalsy();
             done();
         });
@@ -142,7 +144,7 @@ test("should remove expense from database", (done)=>{
 });
 
 test("should update expense from database", (done)=>{
-    const store = createMockStore();
+    const store = createMockStore(defaulAuthState);
     store.dispatch(startEditExpense(expenses[0].id, { description: "boo" })).then(()=>{
         const action = store.getActions();
 
@@ -152,7 +154,7 @@ test("should update expense from database", (done)=>{
             updates: { description: "boo" }
         });
 
-        return database.ref(`expenses/${expenses[0].id}`).once("value").then((snapshot)=>{
+        return database.ref(`users/${uid}/expenses/${expenses[0].id}`).once("value").then((snapshot)=>{
             expect(snapshot.val().description).toBe("boo");
             done();
         });
